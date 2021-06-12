f80f64efa8f9de06e4fbd88245ff54f7f2f24d0d
Tanasya Aulia <Tanasyaaulia@gmail.com>
2021-06-12 20:02:40 +0800

    Operasi Tipe Data String

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Main {
     public static void main (String[]args){
         
@@ -15,5 +17,22 @@ public class Main {
         String uniskaString = new String (uniskaChar);
         System.out.println (uniskaChar);
         System.out.println (uniskaString);
+        
+        String namaLengkap = namaDepan + " " + namaBelakang;
+        System.out.println (namaLengkap);
+        System.out.println (namaLengkap.length());
+        System.out.println (namaLengkap.indexOf("sya"));
+        System.out.println (namaLengkap.substring(8));
+        System.out.println (namaLengkap.substring(2,7));
+        System.out.println (namaLengkap.replace("Tanasya","Nesha"));
+        System.out.println (namaLengkap.toLowerCase());
+        System.out.println (namaLengkap.toUpperCase());
+        System.out.println (namaLengkap.length());
+        System.out.println (namaLengkap.charAt(2));
+
+        String[] namaArray = namaLengkap.split (" ");
+
+        for (String nama : namaArray)
+            System.out.println(nama);
     }
 }
